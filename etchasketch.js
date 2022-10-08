72cc1096c540b462a18a73ba00ac979d48d2d453
Jamie <nordio.jamie@gmail.com>
2022-10-08 14:30:35 -0700

    add black and eraser buttons with functionality

--- etchasketch.js
+++ etchasketch.js
@@ -1,5 +1,7 @@
 const grid = document.querySelector("#grid");
 
+let mode = "black";
+
 for (let i = 1; i <= 16*16; i++) {
     let pixel = document.createElement("div");
     pixel.className = "pixel";
@@ -10,9 +12,23 @@ for (let i = 1; i <= 16*16; i++) {
 const pixels = document.querySelectorAll(".pixel");
 pixels.forEach(pixel => 
     pixel.addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = "black";
+        if (mode === "black") {
+            event.target.style.backgroundColor = "black";
+        } else if (mode === "eraser") {
+            event.target.style.backgroundColor = "#F8F0E3";
+        } 
     })   
 );
+
+const black = document.getElementById("black")
+black.addEventListener("click", () => {
+    mode = "black";
+});
+
+const eraser = document.getElementById("eraser");
+eraser.addEventListener("click", () => {
+    mode = "eraser";
+});
 
 const clear = document.getElementById("clear");
 clear.addEventListener("click", () => {
